dcb436770a6adf4a1f915c82c5317d3901936587
Qin Wan <rogerwq@gmail.com>
2024-12-18 14:14:45 +0900

    change AppInfo author to Chiral

--- crates/tui/src/constants.rs
+++ crates/tui/src/constants.rs
@@ -1,5 +1,5 @@
 pub const APP_NAME: &str = "silva";
-pub const APP_INFO: app_dirs2::AppInfo = app_dirs2::AppInfo{name: APP_NAME, author: "Qin Wan"};
+pub const APP_INFO: app_dirs2::AppInfo = app_dirs2::AppInfo{name: APP_NAME, author: "Chiral"};
 
 pub const FILENAME_ACCOUNTS: &str = "accounts.toml";
 pub const FILENAME_SETTINGS: &str = "settings.toml";
