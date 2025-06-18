821ba96e445dd5c9cad01c6b820150515c34d904
Kal-El-pt2 <arivwalia@gmail.com>
2025-06-18 10:18:30 +0530

    build: protoc compilation fixed

--- crates/providers/rust-client/build.rs
+++ crates/providers/rust-client/build.rs
@@ -1,11 +1,14 @@
+use std::env;
+use std::path::Path;
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .build_client(true)
-        .build_server(false)
-        .out_dir(std::env::var("OUT_DIR")?)
-        .compile(
-            &["proto/chiral.proto"],
-            &["proto"],
-        )?;
+
+    let proto_path = "proto/chiral.proto";
+
+    if !Path::new(proto_path).exists() {
+        panic!("Proto file '{}' does not exist!", proto_path);
+    }
+
+    tonic_build::compile_protos(proto_path)?;
     Ok(())
 }
